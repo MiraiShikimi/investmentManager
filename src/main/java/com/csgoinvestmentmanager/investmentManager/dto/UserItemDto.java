6c1df19524bf59964dc5191aad2cedc81f66334e
MiraiShikimi <miraishikimi@gmail.com>
2022-08-06 15:36:15 +0200

    fixin the updating

--- src/main/java/com/csgoinvestmentmanager/investmentManager/dto/UserItemDto.java
+++ src/main/java/com/csgoinvestmentmanager/investmentManager/dto/UserItemDto.java
@@ -1,13 +1,17 @@
 package com.csgoinvestmentmanager.investmentManager.dto;
 
 import com.csgoinvestmentmanager.investmentManager.model.CSGOItem;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserItemDto implements Serializable {
 
     private CSGOItem csgoItem;
