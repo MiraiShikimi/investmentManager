62d78ab2179b3477b8e0e957fc912d0f90f91e99
MiraiShikimi <miraishikimi@gmail.com>
2022-12-04 15:54:26 +0100

    fiexed the base url, for CORS policy

--- src/main/java/com/csgoinvestmentmanager/investmentManager/InvestmentManagerApplication.java
+++ src/main/java/com/csgoinvestmentmanager/investmentManager/InvestmentManagerApplication.java
@@ -85,7 +85,7 @@ public class InvestmentManagerApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200","csgoitemmenager-production.up.railway.app/"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200","https://steam-invest-tracker.web.app/"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
